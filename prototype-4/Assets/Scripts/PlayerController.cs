f9e92bfdf33df9141e3ed8e4be91dcecb487f54d
candy67-123 <zizhao4@s.sfusd.edu>
2023-03-07 14:30:36 -0800

    test for enemy and power up

--- prototype-4/Assets/Scripts/PlayerController.cs
+++ prototype-4/Assets/Scripts/PlayerController.cs
@@ -34,4 +34,12 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("collided with: " + collision.gameObject.name + "with powerup set to" + hasPowerup);
+        }
+    }
+
 }
