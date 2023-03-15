07bad6408fd949565c2d350c9980265152596a83
candy67-123 <zizhao4@s.sfusd.edu>
2023-03-15 10:51:43 -0700

    destroy enemies if they fall off

--- prototype-4/Assets/Scripts/SpawnManager.cs
+++ prototype-4/Assets/Scripts/SpawnManager.cs
@@ -6,21 +6,26 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWare(3);
+        SpawnEnemyWave(3);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
 
+        if(enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
     }
 
-    void SpawnEnemyWare(int enemiesToSpawn)
+    void SpawnEnemyWave(int enemiesToSpawn)
     {
         for (int i = 0; i < enemiesToSpawn; i++)
         {
